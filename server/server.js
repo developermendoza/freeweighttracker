551daceef6b5a12c3c38d988719db32b67721c0d
Jose Mendoza <developermendoza@gmail.com>
2021-01-21 16:56:44 -0800

    fix but on server.js

--- server/server.js
+++ server/server.js
@@ -34,10 +34,14 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users)
-app.use("/api/weights", weights)
+app.use("/api/weights", weights);
+
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 
   const port = process.env.PORT || 5000;
   app.listen(port, () => console.log(`Server is up and running on port ${port}`));
+
+
 
   app.use(express.static(path.join(__dirname, 'build')));
 
@@ -45,4 +49,4 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(9000)
