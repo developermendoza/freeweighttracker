604ccd0c47cddb44093f7447a9333ae5baf7e902
Jose Mendoza <developermendoza@gmail.com>
2021-01-25 15:04:25 -0800

    add condition on server.js to launch app

--- server/server.js
+++ server/server.js
@@ -36,17 +36,25 @@ require("./config/passport")(passport);
 app.use("/api/users", users)
 app.use("/api/weights", weights);
 
-app.get('/', (req, res) => { res.send('Hello from Express!')});
+// app.get('/', (req, res) => { res.send('Hello from Express!')});
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 
   const port = process.env.PORT || 5000;
   app.listen(port, () => console.log(`Server is up and running on port ${port}`));
 
 
 
-  app.use(express.static(path.join(__dirname, 'build')));
+//   app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.listen(9000)
+// app.listen(9000)
