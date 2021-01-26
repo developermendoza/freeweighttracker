6e45fc976d1322c3ca6e7b4982d41adc28b1066b
Jose Mendoza <developermendoza@gmail.com>
2021-01-26 11:17:04 -0800

    change server.js

--- server/server.js
+++ server/server.js
@@ -37,11 +37,14 @@ app.use("/api/users", users)
 app.use("/api/weights", weights);
 
 // app.get('/', (req, res) => { res.send('Hello from Express!')});
+
+// Serve static assets if in production
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
+  //set static folder
+  app.use(express.static('client/build'));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
