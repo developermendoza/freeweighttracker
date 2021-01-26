1ad405224c3ed9b8c8a82bcc09fd2dd60c2962a3
Jose Mendoza <developermendoza@gmail.com>
2021-01-26 15:08:42 -0800

    testing

--- server.js
+++ server.js
@@ -37,19 +37,14 @@ app.use("/api/users", users)
 app.use("/api/weights", weights);
 
 // Serve static assets if in production
-// if(process.env.NODE_ENV === "production"){
-//   //set static folder
-//   app.use(express.static('client/build'));
+if(process.env.NODE_ENV === "production"){
+  //set static folder
+  app.use(express.static('client/build'));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
   const port = process.env.PORT || 5000;
 
