9803625e553464ce8d8a515192824993cf88ab9a
Jose Mendoza <developermendoza@gmail.com>
2021-01-26 16:19:36 -0800

    add file for production keys

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 // Routes
-app.use("/api/users", users)
+app.use("/api/users", users);
 app.use("/api/weights", weights);
 
 // Serve static assets if in production
@@ -41,7 +41,7 @@ if(process.env.NODE_ENV === "production"){
   //set static folder
   app.use(express.static('client/build'));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 }
